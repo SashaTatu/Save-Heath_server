51cfbde1aff56f6cb7b57681b2902a30b6ceb6df
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-04-11 22:59:54 +0300

    видалення повідомлення що відгук відправлено

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/send-feedback', async (req, res) => {
         const data = await telegramResponse.json();
 
         if (data.ok) {
-            res.json({ status: 'Відгук надіслано!' });
+            res.json();
         } else {
             console.error('Помилка Telegram:', data);
             res.status(500).json({ status: 'Помилка при надсиланні в Telegram' });
